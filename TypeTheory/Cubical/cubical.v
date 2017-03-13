3da70545538dce01d4b6aa6f4e23c388161a33ec
Anders Mörtberg <andersmortberg@gmail.com>
2017-03-13 23:39:26 +0100

    wip

--- TypeTheory/Cubical/cubical.v
+++ TypeTheory/Cubical/cubical.v
@@ -1,166 +1,55 @@
-Require Import UniMath.Foundations.Basics.PartB.
-Require Import UniMath.Foundations.Basics.PartD.
-Require Import UniMath.Foundations.Basics.Propositions.
-Require Import UniMath.Foundations.Basics.Sets.
+Require Import UniMath.Foundations.PartB.
+Require Import UniMath.Foundations.PartD.
+Require Import UniMath.Foundations.Propositions.
+Require Import UniMath.Foundations.Sets.
 
 Require Import UniMath.CategoryTheory.total2_paths.
 Require Import UniMath.CategoryTheory.precategories.
 Require Import UniMath.CategoryTheory.functor_categories.
-Require Import UniMath.CategoryTheory.UnicodeNotations.
+Require Import UniMath.CategoryTheory.Adjunctions.
 Require Import UniMath.CategoryTheory.category_hset.
 Require Import UniMath.CategoryTheory.category_hset_structures.
 Require Import UniMath.CategoryTheory.opp_precat.
 Require Import UniMath.CategoryTheory.whiskering.
 Require Import UniMath.CategoryTheory.equivalences.
-Require Import UniMath.CategoryTheory.AdjunctionHomTypesWeq.
 Require Import UniMath.CategoryTheory.RightKanExtension.
 Require Import UniMath.CategoryTheory.limits.graphs.limits.
 Require Import UniMath.CategoryTheory.limits.graphs.pullbacks.
+Require Import UniMath.CategoryTheory.Presheaf.
+Require Import UniMath.CategoryTheory.ElementsOp.
 
-Local Notation "# F" := (functor_on_morphisms F) (at level 3).
-Local Notation "[ C , D , hs ]" := (functor_precategory C D hs).
-Local Notation "C '^op'" := (opp_precat C) (at level 3, format "C ^op").
-Local Notation "'PSh' C" := (* (functor (opp_precat C) HSET) (at level 3). *)
-  [C^op,HSET,has_homsets_HSET] (at level 3).
-Arguments nat_trans_comp {_ _ _ _ _} _ _.
-Local Notation "α • β" := (nat_trans_comp α β) (at level 50, format "α  •  β", left associativity).
-
-(** Subobject classifier in a presheaf *)
-Section omega_psh.
-
-Context {C : precategory}.
-
-Definition sieve_def (c : C) : UU.
-Proof.
-  use total2.
-  - apply (Π (x : C) (f : C⟦x,c⟧), hProp).
-  - intros S.
-    apply (Π (x : C) (f : C⟦x,c⟧), S x f → Π (y : C) (f' : C⟦y,x⟧), S y (f' ;; f)).
-Defined.
-
-Lemma isaset_sieve (c : C) : isaset (sieve_def c).
-Proof.
-use isaset_total2.
-- repeat (apply impred_isaset; intro); apply isasethProp.
-- intros S; simpl; repeat (apply impred_isaset; intro); apply isasetaprop, propproperty.
-Qed.
-
-Definition sieve (c : C) : HSET := (sieve_def c,,isaset_sieve c).
-
-Definition omega_psh : PSh C.
-Proof.
-use mk_functor.
-- mkpair.
-  + apply sieve.
-  + simpl; intros a b f [S hS].
-    mkpair.
-    * intros y g.
-      apply (S y (g ;; f)).
-    * abstract (intros y g H z h; simpl; rewrite <- assoc; apply hS, H).
-- split.
-  + intros x; apply funextfun; intros [S hS]; simpl.
-    apply subtypeEquality; simpl.
-    * intros X; repeat (apply impred; intro); apply propproperty.
-    * now repeat (apply funextsec; intro); rewrite id_right.
-  + intros x y z f g; apply funextfun; intros [S hS]; simpl.
-    apply subtypeEquality; simpl.
-    * intros X; repeat (apply impred; intro); apply propproperty.
-    * now repeat (apply funextsec; intro); rewrite <- assoc.
-Defined.
-
-End omega_psh.
-
-(** Category of elements of a presheaf *)
-Section cat_of_elems_def.
-
-Context {C : precategory} (hsC : has_homsets C) (F : PSh C).
-
-Definition cat_of_elems_ob_mor : precategory_ob_mor.
-Proof.
-exists (Σ (c : C), pr1 F c : hSet).
-intros a b.
-apply (Σ (f : C⟦pr1 a,pr1 b⟧), # (pr1 F) f (pr2 b) = pr2 a).
-Defined.
-
-Definition cat_of_elems_data : precategory_data.
-Proof.
-exists cat_of_elems_ob_mor.
-split.
-+ intros a.
-  exists (identity _).
-  abstract (now rewrite (functor_id F)).
-+ intros a b c f g.
-  exists (pr1 f ;; pr1 g).
-  abstract (eapply pathscomp0;
-      [apply (toforallpaths _ _ _ (functor_comp F _ _ _ (pr1 g) (pr1 f)) (pr2 c))|];
-      eapply pathscomp0; [|apply (pr2 f)]; cbn;
-      apply maponpaths, (pr2 g)).
-Defined.
-
-Lemma eq_mor_cat_of_elems (a b : cat_of_elems_data) (f g : _ ⟦a,b⟧) :
-  pr1 f = pr1 g → f = g.
-Proof.
-now apply subtypeEquality; intros x; apply setproperty.
-Qed.
-
-Definition cat_of_elems : precategory.
-Proof.
-exists cat_of_elems_data.
-abstract (repeat split; intros;
-          apply subtypeEquality; try (intro; apply setproperty); simpl;
-            [ apply id_left | apply id_right | apply assoc]).
-Defined.
-
-Lemma has_homsets_cat_of_elems : has_homsets cat_of_elems.
-Proof.
-intros a b.
-apply isaset_total2.
-+ now apply has_homsets_opp, hsC.
-+ now intros f; apply isasetaprop, setproperty.
-Qed.
-
-End cat_of_elems_def.
-
-Local Notation "∫ F" := (cat_of_elems F) (at level 3).
+Local Open Scope cat.
 
 Section cat_of_elems_theory.
 
 Context {C : precategory} (hsC : has_homsets C).
 
 Definition nat_trans_cat_of_elems
-  {F G : PSh C} (α : nat_trans (pr1 F) (pr1 G)) : functor (∫ F) (∫ G).
+  {F G : PreShv C} (α : nat_trans (pr1 F) (pr1 G)) : functor (∫ F) (∫ G).
 Proof.
-mkpair.
-- mkpair.
-  + intros X; apply (pr1 X,, α (pr1 X) (pr2 X)).
-  + intros a b f.
-    exists (pr1 f).
-    abstract (
-      eapply pathscomp0;
-        [ eapply pathsinv0,
-                 (toforallpaths _ _ _ (nat_trans_ax α _ _ (pr1 f)) (pr2 b)) |];
-      unfold compose; simpl; apply maponpaths, (pr2 f)).
-- abstract (now split; intros ?; intros; apply eq_mor_cat_of_elems).
+apply cat_of_elems_on_nat_trans.
+assumption.
 Defined.
 
 (* Lemma nat_trans_cat_of_elems_id *)
 (*   {F : PSh C} : nat_trans_cat_of_elems (nat_trans_id (pr1 F)) = functor_identity _. *)
 
-Definition subst_functor {F G : PSh C} (α : nat_trans (pr1 F) (pr1 G)) :
-  functor (PSh (∫ G)) (PSh (∫ F)).
+Definition subst_functor {F G : PreShv C} (α : nat_trans (pr1 F) (pr1 G)) :
+  functor (PreShv (∫ G)) (PreShv (∫ F)).
 Proof.
-apply (pre_composition_functor _ _ _ (has_homsets_opp (has_homsets_cat_of_elems hsC G))).
-now apply functor_opp, nat_trans_cat_of_elems.
+use pre_composition_functor.
+- apply has_homsets_opp, has_homsets_cat_of_elems, hsC.
+- now apply functor_opp, nat_trans_cat_of_elems.
 Defined.
 
-Lemma is_left_adjoint_subst_functor {F G : PSh C} (α : nat_trans (pr1 F) (pr1 G)) :
+Lemma is_left_adjoint_subst_functor {F G : PreShv C} (α : nat_trans (pr1 F) (pr1 G)) :
   is_left_adjoint (subst_functor α).
 Proof.
 use (RightKanExtension_from_limits _ _ _ LimsHSET). (* apply is slow here... *)
 Defined.
 
 (* This name is maybe not very good *)
-Definition π {F G : PSh C} (α : nat_trans (pr1 F) (pr1 G)) :=
+Definition π {F G : PreShv C} (α : nat_trans (pr1 F) (pr1 G)) :=
    right_adjoint (is_left_adjoint_subst_functor α).
 
 End cat_of_elems_theory.
@@ -169,33 +58,39 @@ Section types.
 
 Context {C : precategory} (hsC : has_homsets C).
 
-Definition TypeIn (Γ : PSh C) : UU := PSh (∫ Γ).
+Definition TypeIn (Γ : PreShv C) : UU := PreShv (∫ Γ).
 
 Local Notation "Γ ⊢" := (TypeIn Γ) (at level 3).
 
 (* Given Γ ⊢ A and a substitution σ : Δ → Γ we get Δ ⊢ Aσ *)
-Lemma subst_type {Γ Δ : PSh C} (A : Γ ⊢) (σ : nat_trans (pr1 Δ) (pr1 Γ)) : Δ ⊢.
+Lemma subst_type {Γ Δ : PreShv C} (A : Γ ⊢) (σ : nat_trans (pr1 Δ) (pr1 Γ)) : Δ ⊢.
 Proof.
 apply (subst_functor hsC σ A).
 Defined.
 
-Lemma subst_type_id (Γ : PSh C) (A : Γ ⊢) :
+(* A1 = A *)
+Lemma subst_type_id (Γ : PreShv C) (A : Γ ⊢) :
   subst_type A (nat_trans_id (pr1 Γ)) = A.
 Proof.
-apply functor_eq.
-apply has_homsets_HSET.
-simpl.
-assert (foo : pr1 (functor_composite_data
-    (functor_opp_data (nat_trans_cat_of_elems (nat_trans_id (pr1 Γ)))) (pr1 A)) =
-  pr1 (pr1 A)).
-cbn.
-apply funextfun; intro a.
-apply maponpaths.
-apply pathsinv0.
-apply tppr.
-apply (total2_paths foo).
-cbn.
+apply (functor_eq _ _ has_homsets_HSET).
+use functor_data_eq.
+- intros [c1 c2]; apply idpath.
+- intros [a1 a2] [b1 b2] f; cbn.
+  now apply maponpaths, subtypeEquality; [intros x; apply setproperty|].
+Qed.
 
+(* TODO: swap? *)
+(* A(rho delta) = (Arho)delta *)
+Lemma subst_type_comp (Γ Delta Theta : PreShv C) (s1 : nat_trans (pr1 Theta) (pr1 Delta))
+  (s2 : nat_trans (pr1 Delta) (pr1 Γ)) (A : Γ ⊢) :
+  subst_type A (nat_trans_comp _ _ _ s1 s2) = subst_type (subst_type A s2) s1.
+Proof.
+apply (functor_eq _ _ has_homsets_HSET).
+use functor_data_eq.
+- intros [c1 c2]; apply idpath.
+- intros [a1 a2] [b1 b2] f; cbn.
+  now apply maponpaths, subtypeEquality; [intros x; apply setproperty|].
+Qed.
 
 End types.
 
@@ -204,7 +99,7 @@ Section Glue.
 Local Notation "Γ ⊢" := (TypeIn Γ) (at level 3).
 
 Variables (C : precategory) (hsC : has_homsets C).
-Variables (Γ Δ : PSh C) (σ : nat_trans (pr1 Δ) (pr1 Γ)).
+Variables (Γ Δ : PreShv C) (σ : nat_trans (pr1 Δ) (pr1 Γ)).
 
 Variables (A : Γ ⊢) (T : Δ ⊢).
 
@@ -213,21 +108,21 @@ Let Aσ : Δ ⊢ := subst_type hsC A σ.
 Variables (w : nat_trans (pr1 T) (pr1 Aσ)).
 
 (* This should be proved more directly for efficiency *)
-Lemma PullbacksPSh : Pullbacks [(∫ Γ)^op, HSET, has_homsets_HSET].
+Lemma PullbacksPreShv : Pullbacks [(∫ Γ)^op, HSET, has_homsets_HSET].
 Proof.
 apply Pullbacks_from_Lims, LimsFunctorCategory, LimsHSET.
 Defined.
 
 Definition Glue' : Γ ⊢.
 Proof.
-set (πAσ := π hsC σ Aσ : PSh (∫ Γ)).
-set (πT := π hsC σ T : PSh (∫ Γ)).
+set (πAσ := π hsC σ Aσ : PreShv (∫ Γ)).
+set (πT := π hsC σ T : PreShv (∫ Γ)).
 transparent assert (f1 : (_⟦πT,πAσ⟧)).
   apply (φ_adj _ _ _ (is_left_adjoint_subst_functor hsC σ)).
   apply (nat_trans_comp (counit_from_left_adjoint (is_left_adjoint_subst_functor hsC σ) T) w).
 transparent assert (f2 : (_⟦A,πAσ⟧)).
   apply (φ_adj _ _ _ (is_left_adjoint_subst_functor hsC σ) (identity _)).
-apply (PullbackObject _ (PullbacksPSh _ _ _ f1 f2)).
+apply (PullbackObject _ (PullbacksPreShv _ _ _ f1 f2)).
 Defined.
 
 End Glue.
@@ -241,9 +136,9 @@ Local Notation "'Id'" := (functor_identity C).
 Variable (p_F : nat_trans F Id).
 Variable (e0 e1 : nat_trans Id F).
 
-Variable (Γ : PSh C).
+Variable (Γ : PreShv C).
 
-Definition Γplus : PSh C.
+Definition Γplus : PreShv C.
 Proof.
 mkpair.
 - mkpair.
