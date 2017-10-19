6aad488fd568722952c707416498054d53fc1cd8
Anders Mörtberg <andersmortberg@gmail.com>
2017-10-20 00:09:03 +0200

    make things compile

--- TypeTheory/Cubical/Compositions.v
+++ TypeTheory/Cubical/Compositions.v
@@ -4,8 +4,8 @@ Require Import UniMath.CategoryTheory.total2_paths.
 Require Import UniMath.CategoryTheory.Categories.
 Require Import UniMath.CategoryTheory.functor_categories.
 Require Import UniMath.CategoryTheory.Adjunctions.
-Require Import UniMath.CategoryTheory.category_hset.
-Require Import UniMath.CategoryTheory.category_hset_structures.
+Require Import UniMath.CategoryTheory.categories.category_hset.
+Require Import UniMath.CategoryTheory.categories.category_hset_structures.
 Require Import UniMath.CategoryTheory.opp_precat.
 Require Import UniMath.CategoryTheory.whiskering.
 Require Import UniMath.CategoryTheory.equivalences.
